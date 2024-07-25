1e52fb66fd67e55c533c2f71419ab02fceca500a
Christopher Schaefer <disks86@gmail.com>
2024-07-24 21:46:37 -0400

    Added Unicode string
    Added Unicode string.

---
+++ Core/UnicodeString.cpp
@@ -0,0 +1,2 @@
+
+#include "UnicodeString.h"
---
+++ Core/UnicodeString.h
@@ -0,0 +1,17 @@
+//
+// Created by disks on 7/24/2024.
+//
+
+#ifndef REGULARLIBRARY_UNICODESTRING_H
+#define REGULARLIBRARY_UNICODESTRING_H
+
+#include "Export.h"
+#include "List.h"
+
+namespace Core {
+
+    typedef List<wchar_t> UnicodeString;
+
+} // Core
+
+#endif //REGULARLIBRARY_UNICODESTRING_H
